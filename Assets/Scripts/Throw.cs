73c34280a604c2880f3fce682decf48502508fbb
miyakp098 <ybtkeppen813@gmail.com>
2023-12-19 17:54:58 +0900

    力をクリックしてからの秒数に変更

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     public GameObject prefab; // 発射するプレハブ
+    private float clickTime; // クリックした時間を記録
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(0)) // マウスを押した瞬間
+        {
+            clickTime = Time.time; // 現在の時間を記録
+        }
+
         if (Input.GetMouseButtonUp(0)) // マウスを離した瞬間
         {
+            float clickDuration = Time.time - clickTime; // クリックの持続時間を計算
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.z = 0; // Z軸は無視
 
             Vector2 direction = (mousePosition - transform.position).normalized; // 発射方向を計算
-            float distance = Vector3.Distance(transform.position, mousePosition); // オブジェクトとマウス位置の間の距離に基づいて力を計算
 
             GameObject projectile = Instantiate(prefab, transform.position, Quaternion.identity); // プレハブをインスタンス化
-            projectile.GetComponent<Rigidbody2D>().velocity = direction * distance * 2f; // プレハブを発射
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * clickDuration * 10f; // 時間に基づいて力を計算し、プレハブを発射
         }
     }
 }
