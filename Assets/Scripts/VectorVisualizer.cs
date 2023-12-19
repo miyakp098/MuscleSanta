0af248a63dc2bcdcde6b2432ae9cc7662776502a
miyakp098 <ybtkeppen813@gmail.com>
2023-12-19 19:14:16 +0900

    クリックしてる時矢印表示
    他
    スクリプトの名前
    Throw → Shooter

---
+++ Assets/Scripts/VectorVisualizer.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class VectorVisualizer : MonoBehaviour
+{
+    public GameObject arrowPrefab; // 矢印のプレハブ
+    private GameObject currentArrow; // 現在表示されている矢印
+    private bool isDragging = false; // ドラッグ中かどうかのフラグ
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // マウスを押した瞬間
+        {
+            StartDragging();
+        }
+
+        if (isDragging)
+        {
+            UpdateArrowPositionAndRotation();
+        }
+
+        if (Input.GetMouseButtonUp(0)) // マウスを離した瞬間
+        {
+            EndDragging();
+        }
+    }
+
+    private void StartDragging()
+    {
+        isDragging = true;
+        Vector3 startPosition = transform.position;
+        currentArrow = Instantiate(arrowPrefab, startPosition, Quaternion.identity);
+    }
+
+    private void UpdateArrowPositionAndRotation()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0; // Z軸は無視
+
+        Vector3 direction = mousePosition - transform.position;
+        currentArrow.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction); // 矢印の方向を設定
+        // currentArrow.transform.localScale の設定は削除し、矢印の大きさは変更しない
+    }
+
+    private void EndDragging()
+    {
+        isDragging = false;
+        Destroy(currentArrow); // 矢印を削除
+    }
+}
